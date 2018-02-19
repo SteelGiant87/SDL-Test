2fb2ae7d489ba7113ae6e1cbdf713a0abf719cc9
SteelGiant87 <SteelGiant87@gmail.com>
2018-02-19 22:27:59 +0000

    Applied code formatting settings

--- SDLTest/SDLTest.cpp
+++ SDLTest/SDLTest.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <SDL.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
 	} else {
